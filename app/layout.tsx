4d1c8a55b842135259513276ae05a28ddb3bbf9d
riiz08 <mhmdrizki0812@gmail.com>
2025-06-16 04:59:26 +0700

    add ahrefs monitoring

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,19 +49,11 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
         />
-        <Script id="histats-script" strategy="afterInteractive">
-          {`
-            var _Hasync= _Hasync|| [];
-            _Hasync.push(['Histats.start', '1,4956141,4,511,95,18,00000000']);
-            _Hasync.push(['Histats.fasi', '1']);
-            _Hasync.push(['Histats.track_hits', '']);
-            (function() {
-              var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-              hs.src = ('//s10.histats.com/js15_as.js');
-              (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-            })();
-          `}
-        </Script>
+        <Script
+          src="https://analytics.ahrefs.com/analytics.js"
+          data-key="OtQz1d0e2E+0YDRYqKFR/A"
+          async
+        />
       </head>
       <body
         className={clsx(
@@ -76,22 +68,6 @@ export default function RootLayout({
             <AdsenseSlot slot="5978949902" />
             {children}
           </main>
-          <div
-            id="histats_counter"
-            className="mt-2 absolute left-1/2 transform -translate-x-1/2"
-          />
-
-          {/* Fallback untuk non-JS browser */}
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <noscript>
-            <a href="/" target="_blank">
-              <img
-                src="//sstatic1.histats.com/0.gif?4956141&101"
-                alt="histats"
-                style={{ border: 0 }}
-              />
-            </a>
-          </noscript>
           <div className="fixed bottom-5 right-5 p-2 z-[999] rounded-full shadow-none">
             <ThemeSwitch />
           </div>
