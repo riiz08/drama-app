84974c713a9d05b4539d630253a721155ea6eb1a
riiz08 <mhmdrizki0812@gmail.com>
2025-06-17 06:47:20 +0700

    adnanksao

--- app/latest-update/page.tsx
+++ app/latest-update/page.tsx
@@ -10,7 +10,7 @@ export const metadata = getSeoMetadata({
   title: "Nonton Drama Melayu Terbaru 2025 | Streaming Gratis di MangEakk",
   description:
     "Streaming drama Melayu terbaru dari Malaysia, Brunei, dan Indonesia. Nikmati tayangan kualitas HD dengan subtitle, tanpa iklan!",
-  url: "https://mangeakkk.my.id/popular",
+  url: "https://mangeakkk.my.id/latest-update",
 });
 
 const Page = async () => {
