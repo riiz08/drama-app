28068f06ca6f62543fff0b90238860e839ea8ba6
riiz08 <mhmdrizki0812@gmail.com>
2025-07-25 11:09:10 +0700

    adsadnmdkasksadm

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,17 +1,11 @@
 // app/api/revalidate/route.ts
 import { revalidatePath } from "next/cache";
+import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   const { path } = await req.json();
 
-  if (!path) {
-    return new Response(JSON.stringify({ error: "Missing path" }), {
-      status: 400,
-    });
-  }
+  revalidatePath(path);
 
-  revalidatePath(path); // e.g. /page/1
-  return new Response(JSON.stringify({ revalidated: true }), {
-    status: 200,
-  });
+  return NextResponse.json({ revalidate: path });
 }
