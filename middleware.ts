2116a5bd08406ae2cb3add8cf90660178315e130
riiz08 <mhmdrizki0812@gmail.com>
2025-07-27 07:23:01 +0700

    add middleware rule

--- middleware.ts
+++ middleware.ts
@@ -3,18 +3,10 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const maintenanceMode = false; // Ubah jadi `false` kalau mau normal kembali
+  const url = request.nextUrl;
 
-  const url = request.nextUrl.clone();
-
-  if (
-    maintenanceMode &&
-    !url.pathname.startsWith("/maintenance") &&
-    !url.pathname.startsWith("/_next") &&
-    !url.pathname.startsWith("/favicon.ico")
-  ) {
-    url.pathname = "/maintenance";
-
+  if (url.searchParams.has("bypass")) {
+    url.searchParams.delete("bypass");
     return NextResponse.redirect(url);
   }
 
