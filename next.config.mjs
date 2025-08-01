52e3452b4749bd811ca565aa3ae501f07608cee5
riiz08 <mhmdrizki0812@gmail.com>
2025-08-01 19:36:43 +0700

    csp conflict fix 2

--- next.config.mjs
+++ next.config.mjs
@@ -14,29 +14,6 @@ const nextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: `
-default-src * data: blob: 'unsafe-inline' 'unsafe-eval';
-script-src * data: blob: 'unsafe-inline' 'unsafe-eval';
-style-src * data: blob: 'unsafe-inline';
-img-src * data: blob:;
-frame-src *;
-connect-src *;
-font-src * data:;
-media-src *;
-object-src *;
-`.replace(/\n/g, ""),
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
